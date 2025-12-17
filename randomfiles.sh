82b351e34e7ba3438d56cfe0573dec686d6e788a
georgeflanagin <milesdavis@billevans>
2025-12-17 16:05:17 -0500

    updated.

--- randomfiles.sh
+++ randomfiles.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -e
 
+# Best to [re]build vmtouch for this distro. It is not in
+# the core OS.
+if ! command -v vmtouch >/dev/null 2>&1; then
+    echo "vmtouch not installed. Installing it"
+    sudo dnf install -y gcc make git
+    git clone https://github.com/hoytech/vmtouch.git
+    cd vmtouch
+    make
+    sudo install -m 0755 vmtouch /usr/local/bin/
+fi
+
+# pv has the same limitation.
+if ! command -v pv >/dev/null 2>&1; then
+    echo "pv not installed. Installing it."
+    dnf -y install pv
+fi
+
 if [ -z "$2" ]; then
     echo "Usage: $0 {numfiles} {user@ipaddress}"
     exit 1
@@ -12,14 +29,16 @@ DEST="$2"
 /usr/bin/time -v python ./randomfiles.py -n "$NUM"
 
 # Load them into cache
-find . -name '*.iotest' -print0 | xargs -0 vmtouch -t
+find . -type f -name '*.iotest' -print0 | xargs -0 vmtouch -t
+find . -type f -name '*.iotest' -print0 | xargs -0 vmtouch
 
 
-# probably the fastest?
-/usr/bin/time -v rsync ./*.iotest "$2:."
 
-# different stats
-pv ./*.iotest | "$2" "cat > /dev/null"
+# Stats.
+find . -type f -name '*.iotest' -print0 \
+    | sort -z \
+    | xargs -0 pv -ra8tpe -i 1 \
+    | ssh -T -o BatchMode=yes "$2" "cat > /dev/null"
 
 
 
